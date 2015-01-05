b619cc2e37505ce5cb6fe15aa8b003444c3d0c8f
EricRho <eric.rho.87@gmail.com>
2015-01-05 11:20:01 -0500

    change some layouts. nothing big

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :records
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
   # authenticated :user do
   #   root :to => "records#index"
