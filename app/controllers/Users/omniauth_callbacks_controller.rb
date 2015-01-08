edb9041ff4e8d3152b152d4787c4b8578f49c40d
EricRho <eric.rho.87@gmail.com>
2015-01-08 16:21:10 -0500

    about to impliment omniauth

--- app/controllers/Users/omniauth_callbacks_controller.rb
+++ app/controllers/Users/omniauth_callbacks_controller.rb
@@ -1,4 +1,30 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def authenticate
+    @discogs = Discogs::Wrapper.new("Test Oath")
+    request_data = @discogs.get_request_token("vVqZWXDJOLmvpvMCoQly", "KNnWkTvicsySetHQDMYRZXYfehAZlNGS", "http://localhost:3000/callback")
+
+    session[:request_token] = request_data[:request_token]
+
+    redirect_to request_data[:authorize_url]
+  end
+
+  def callback
+    @discogs = Discogs::Wrapper.new("Test Oauth")
+    request_token = session[:request_token]
+    verifier = params[:oauth_verifier]
+    access_token = @discogs.authenticate(request_token, verifier)
+
+    session[:request_token] = nil
+    session[:access_token] = access_token
+
+    @discogs = access_token = access_token
+  end
+
+  def another_action
+    @discogs = Discogs::Wrapper.new("Test Oauth", session[:access_token])
+  end
+
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
